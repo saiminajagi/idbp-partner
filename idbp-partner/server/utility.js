3241930b928c5181024eed34bc1662f49e8d83e9
Tushar MALCHAPURE <tmalchap@in.ibm.com>
2019-06-12 11:46:26 +0530

    quick signup problem resolved

--- idbp-partner/server/utility.js
+++ idbp-partner/server/utility.js
@@ -6,7 +6,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var callCoreBackend = function(options,cb){
     try{
         //console.log("Options: "+options);
-        console.log("calcorebackend called");
+        console.log("callcorebackend called");
         request(options,(e,resp,body)=>{
             if(resp != undefined){
                 console.log("response is :"+JSON.stringify(resp));
